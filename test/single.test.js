7915855823f2ab98b8f793c1e60fab6bad64533a
qarachit <cs_rachit@yahoo.com>
2019-05-30 10:52:55 +0530

    jest with selenium webdriver

---
+++ test/single.test.js
@@ -0,0 +1,79 @@
+const webdriver = require('selenium-webdriver');
+const {until} = require('selenium-webdriver');
+const {By} = require('selenium-webdriver');
+
+username= process.env.LT_USERNAME || "<your username>",
+accessKey=  process.env.LT_ACCESS_KEY || "<your accessKey>"
+
+const capabilities = {
+  'build': 'jest-LambdaTest-Single',
+  'browserName': 'chrome',
+  'version':'72.0',
+  'platform': 'WIN10',
+  'video': true,
+  'network': true,
+  'console': true,
+  'visual': true
+};
+
+const getElementById = async (driver, id, timeout = 2000) => {
+  const el = await driver.wait(until.elementLocated(By.id(id)), timeout);
+  return await driver.wait(until.elementIsVisible(el), timeout);
+};
+
+const getElementByName = async (driver, name, timeout = 2000) => {
+  const el = await driver.wait(until.elementLocated(By.name(name)), timeout);
+  return await driver.wait(until.elementIsVisible(el), timeout);
+};
+
+const getElementByXpath = async (driver, xpath, timeout = 2000) => {
+  const el = await driver.wait(until.elementLocated(By.xpath(xpath)), timeout);
+  return await driver.wait(until.elementIsVisible(el), timeout);
+};
+
+describe('webdriver', () => {
+  let driver;
+
+  beforeAll(async () => {
+    driver = new webdriver.Builder()
+      .usingServer('https://'+ username +':'+ accessKey  +'@hub.lambdatest.com/wd/hub')
+      .withCapabilities(capabilities)
+      .build();
+
+    // eslint-disable-next-line no-undef
+    await driver.get(
+      `https://lambdatest.github.io/sample-todo-app/`,
+    );
+  }, 10000);
+
+  afterAll(async () => {
+    await driver.quit();
+  }, 15000);
+
+  test(
+      'test',
+      async () => {
+      
+      const lnk = await getElementByName(driver, 'li1');
+      await lnk.click();
+      
+      const lnk1 = await getElementByName(driver, 'li2');
+      await lnk1.click();
+
+      const inpf = await getElementById(driver, 'sampletodotext');
+      await inpf.clear();
+      await inpf.sendKeys("Yey, Let's add it to list");
+
+      const btn = await getElementById(driver, 'addbutton');
+      await btn.click();
+
+      const output = await getElementByXpath(driver, '//html/body/div/div/div/ul/li[6]/span');
+      const outputVal = await output.getText();
+      console.log(outputVal);
+
+      expect(outputVal).toEqual("Yey, Let's add it to list");
+      
+    },
+    10000,
+  );
+});
